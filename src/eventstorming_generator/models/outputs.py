516da5c793d60b0c0cc186ef29623a5302141db7
ShinSeongJin2 <ShinSeongJin@uengine.org>
2025-05-08 13:09:04 +0900

    일부 출력값 타입 추가

--- src/eventstorming_generator/models/outputs.py
+++ src/eventstorming_generator/models/outputs.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
-from typing import Any
+from pydantic import BaseModel, ConfigDict
+from typing import Any, Dict
+
+class EsValueModel(BaseModel):
+    elements: Dict[str, Any] = {}
+    relations: Dict[str, Any] = {}
+    model_config = ConfigDict(extra="allow")
 
 class OutputsModel(BaseModel):
-    esValue: Any = None
+    esValue: EsValueModel = EsValueModel()
